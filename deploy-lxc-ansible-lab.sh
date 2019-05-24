fd5519d2d37778e173021a0887515239e7faaab6
Renato Araujo <renato.biology@gmail.com>
2019-05-24 23:32:32 +0000

    Adding some new functions and a sample hosts file generated via script

--- deploy-lxc-ansible-lab.sh
+++ deploy-lxc-ansible-lab.sh
@@ -43,7 +43,50 @@ done
 
 list-running-containers () {
 
+
+TOTAL=$(lxc-ls -f | grep -v NAME | wc -l)
+
 lxc-ls -f
+echo ""
+echo "-----------------------------------------------------------"
+echo "TOTAL = $TOTAL Containers"
+echo ""
+
+}
+
+
+
+# Creates a hosts file using the list of all currently running lxc containers.
+# Function just in initial stage, still have to add validation structure to first check 
+# if actually we have instances up and running before trying to generate a list of hosts.
+
+
+create-hosts-file () {
+
+local LIST=$(lxc-ls -f | grep -v NAME | cut -c37-47)
+local NUM=$(lxc-ls -f | grep -v NAME | wc -l)
+
+echo "Adding all the $NUM containers to a hosts file on the current directory ----> $PWD"
+echo "[all]" >> hosts
+
+for i in $LIST
+do
+   echo $i >> hosts
+done
+
+}
+
+
+
+
+# add user input validation..
+
+remove-target-container () {
+
+read -p "Please, enter the name of the lxc container you need to kill: " LXC_NAME
+
+lxc-destroy --name $LXC_NAME --force
+echo "Container $LXC_NAME killed."
 
 }
 
@@ -51,12 +94,15 @@ lxc-ls -f
 
 # case/select statement which shows options to user
 
-select TASK in 'Deploy LXC Containers' 'Destroy All LXC Containers' 'List All Running Containers'
+select TASK in 'Deploy LXC Containers' 'Destroy All LXC Containers' 'List All Running Containers' 'Create hosts file' 'List All Running Containers' 'Remove Target Container'
 do
 	case $REPLY in 
                   1) TASK=deploy-containers;;
 		  2) TASK=kill-all;;
                   3) TASK=list-running-containers;;
+                  4) TASK=create-hosts-file;;
+                  5) TASK=list-running-containers;;
+                  6) TASK=remove-target-container;;
         esac
 
 	if [ -n "$TASK" ]
